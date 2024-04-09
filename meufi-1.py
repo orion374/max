d14e6e7da6e7dbffda73df9da2c8f4b77c5e7c82
GolfiArtistryGamer <165471099+GolfiArtistryGamer@users.noreply.github.com>
2024-04-09 15:08:21 +0200

    Add files via upload

---
+++ meufi-1.py
@@ -0,0 +1,184 @@
+#EXTM3U
+#EXTINF:-1 tvg-logo="https://projatol.000webhostapp.com/logo/playlist.png" group-title="IPTV",[COLOR yellow][B]                                                    [[COLOR lime][B]Playlist-French[COLOR yellow]][/B][/COLOR]
+http://google.com
+#EXTINF:-1 tvg-logo="https://projatol.000webhostapp.com/logo/update.png" group-title="Golfi ",[COLOR yellow][B]                                                           [[COLOR magenta][B]Update[COLOR yellow]][/B][/COLOR]
+http://google.com
+#EXTVLCOPT:network-caching=10000
+#EXTINF:-1,FR | NATIONAL GEO SD
+http://sonyiptv.net:8080/ahm/1234/275765
+#EXTINF:-1,FR | NATIONAL GEOGRAPHIC WILD SD
+http://sonyiptv.net:8080/ahm/1234/275766
+#EXTINF:-1,FR | TF1 SD
+http://sonyiptv.net:8080/ahm/1234/275720
+#EXTINF:-1,FR | FRANCE 2 SD
+http://sonyiptv.net:8080/ahm/1234/275721
+#EXTINF:-1,FR | FRANCE 3 SD
+http://sonyiptv.net:8080/ahm/1234/275722
+#EXTINF:-1,FR | FRANCE 4 SD
+http://sonyiptv.net:8080/ahm/1234/275723
+#EXTINF:-1,FR | FRANCE 5 SD
+http://sonyiptv.net:8080/ahm/1234/275724
+#EXTINF:-1,FR | AB1 SD
+http://sonyiptv.net:8080/ahm/1234/275725
+#EXTINF:-1,FR | RTL9 SD
+http://sonyiptv.net:8080/ahm/1234/275726
+#EXTINF:-1,FR | 6TER SD
+http://sonyiptv.net:8080/ahm/1234/275727
+#EXTINF:-1,FR | LCI SD
+http://sonyiptv.net:8080/ahm/1234/275728
+#EXTINF:-1,FR | CANAL+ SD
+http://sonyiptv.net:8080/ahm/1234/275729
+#EXTINF:-1,FR | CANAL+SPORT SD
+http://sonyiptv.net:8080/ahm/1234/275730
+#EXTINF:-1,FR | CANAL + 360 SD
+http://sonyiptv.net:8080/ahm/1234/275731
+#EXTINF:-1,FR | CANAL + FOOT SD
+http://sonyiptv.net:8080/ahm/1234/275732
+#EXTINF:-1,FR | CANAL+ PREMIERE LEAGUE SD
+http://sonyiptv.net:8080/ahm/1234/275733
+#EXTINF:-1,FR | CANAL + TOP14 SD
+http://sonyiptv.net:8080/ahm/1234/275734
+#EXTINF:-1,FR | CANAL + MOTOGP SD
+http://sonyiptv.net:8080/ahm/1234/275735
+#EXTINF:-1,FR | CANAL + INFOSPORT+ SD
+http://sonyiptv.net:8080/ahm/1234/275736
+#EXTINF:-1,FR | CANAL + GOLF+ SD
+http://sonyiptv.net:8080/ahm/1234/275737
+#EXTINF:-1,FR | CANAL + AUTOMOTO LA CHAINE SD
+http://sonyiptv.net:8080/ahm/1234/275738
+#EXTINF:-1,FR | EQUIDIA
+http://sonyiptv.net:8080/ahm/1234/275739
+#EXTINF:-1,FR | L'EQUIPE SD
+http://sonyiptv.net:8080/ahm/1234/275740
+#EXTINF:-1,FR | EUROSPORT 1 SD
+http://sonyiptv.net:8080/ahm/1234/275741
+#EXTINF:-1,FR | EUROSPORT 360 1 SD
+http://sonyiptv.net:8080/ahm/1234/275742
+#EXTINF:-1,FR | EUROSPORT 360 2 SD
+http://sonyiptv.net:8080/ahm/1234/275743
+#EXTINF:-1,FR | EUROSPORT 360 3 SD
+http://sonyiptv.net:8080/ahm/1234/275744
+#EXTINF:-1,FR | EUROSPORT 360 4 SD
+http://sonyiptv.net:8080/ahm/1234/275745
+#EXTINF:-1,FR | BEIN SPORTS MAX 4 SD
+http://sonyiptv.net:8080/ahm/1234/275746
+#EXTINF:-1,FR | BEIN SPORTS MAX 5 SD
+http://sonyiptv.net:8080/ahm/1234/275747
+#EXTINF:-1,FR | BEIN SPORTS MAX 6 SD
+http://sonyiptv.net:8080/ahm/1234/275748
+#EXTINF:-1,FR | BEIN SPORTS MAX 7 SD
+http://sonyiptv.net:8080/ahm/1234/275749
+#EXTINF:-1,FR | BEIN SPORTS MAX 8 SD
+http://sonyiptv.net:8080/ahm/1234/275750
+#EXTINF:-1,FR | BEIN SPORTS MAX 9 SD
+http://sonyiptv.net:8080/ahm/1234/275751
+#EXTINF:-1,FR | CINE+ CLASSIC SD
+http://sonyiptv.net:8080/ahm/1234/275752
+#EXTINF:-1,FR | CINE+ FAMIZ SD
+http://sonyiptv.net:8080/ahm/1234/275753
+#EXTINF:-1,FR | CINE+ EMOTION SD
+http://sonyiptv.net:8080/ahm/1234/275754
+#EXTINF:-1,FR | CINE+ FRISSON SD
+http://sonyiptv.net:8080/ahm/1234/275755
+#EXTINF:-1,FR | OCS MAX SD
+http://sonyiptv.net:8080/ahm/1234/275756
+#EXTINF:-1,FR | OCS GEANTS SD
+http://sonyiptv.net:8080/ahm/1234/275757
+#EXTINF:-1,FR | ACTION SD
+http://sonyiptv.net:8080/ahm/1234/275758
+#EXTINF:-1,FR | SEASONS SD
+http://sonyiptv.net:8080/ahm/1234/275759
+#EXTINF:-1,FR | SERIE CLUB SD
+http://sonyiptv.net:8080/ahm/1234/275760
+#EXTINF:-1,FR | POLAR+ SD
+http://sonyiptv.net:8080/ahm/1234/275761
+#EXTINF:-1,FR | PARAMOUNT CHANNEL SD
+http://sonyiptv.net:8080/ahm/1234/275762
+#EXTINF:-1,FR | NOVELAS TV SD
+http://sonyiptv.net:8080/ahm/1234/275763
+#EXTINF:-1,FR | PARIS PREMIERE SD
+http://sonyiptv.net:8080/ahm/1234/275764
+#EXTINF:-1,FR | RMC DECOUVERTE SD
+http://sonyiptv.net:8080/ahm/1234/275767
+#EXTINF:-1,FR | RMC STORY SD
+http://sonyiptv.net:8080/ahm/1234/275768
+#EXTINF:-1,FR | MUSEUM SD
+http://sonyiptv.net:8080/ahm/1234/275769
+#EXTINF:-1,FR | HISTOIRE TV SD
+http://sonyiptv.net:8080/ahm/1234/275770
+#EXTINF:-1,FR | TOUTE L'HISTOIRE SD
+http://sonyiptv.net:8080/ahm/1234/275771
+#EXTINF:-1,FR | USHUAIA TV SD
+http://sonyiptv.net:8080/ahm/1234/275772
+#EXTINF:-1,FR | CHASSE ET PECHE
+http://sonyiptv.net:8080/ahm/1234/275773
+#EXTINF:-1,FR | TEVA SD
+http://sonyiptv.net:8080/ahm/1234/275774
+#EXTINF:-1,FR | ASTRO VOYANCE SD
+http://sonyiptv.net:8080/ahm/1234/275775
+#EXTINF:-1,FR | CHERIE 25 SD
+http://sonyiptv.net:8080/ahm/1234/275776
+#EXTINF:-1,FR | OLYMPIA TV SD
+http://sonyiptv.net:8080/ahm/1234/275777
+#EXTINF:-1,FR | TV5 MONDE SD
+http://sonyiptv.net:8080/ahm/1234/275778
+#EXTINF:-1,FR | MUSEUM SD
+http://sonyiptv.net:8080/ahm/1234/275779
+#EXTINF:-1,FR | MTV SD
+http://sonyiptv.net:8080/ahm/1234/275780
+#EXTINF:-1,FR | MCM SD
+http://sonyiptv.net:8080/ahm/1234/275781
+#EXTINF:-1,FR | NRJ 12 SD
+http://sonyiptv.net:8080/ahm/1234/275782
+#EXTINF:-1,FR | C8 SD
+http://sonyiptv.net:8080/ahm/1234/275783
+#EXTINF:-1,FR | COMEDIE+ SD
+http://sonyiptv.net:8080/ahm/1234/275784
+#EXTINF:-1,FR | LA CHAINE METEO SD
+http://sonyiptv.net:8080/ahm/1234/275785
+#EXTINF:-1,FR | ALJAZEERA SD
+http://sonyiptv.net:8080/ahm/1234/275786
+#EXTINF:-1,FR | BBC WORLD SD
+http://sonyiptv.net:8080/ahm/1234/275787
+#EXTINF:-1,FR | EURONEWS SD
+http://sonyiptv.net:8080/ahm/1234/275788
+#EXTINF:-1,FR | FRANCE24 SD
+http://sonyiptv.net:8080/ahm/1234/275789
+#EXTINF:-1,FR | FRANCE INFO SD
+http://sonyiptv.net:8080/ahm/1234/275790
+#EXTINF:-1,FR | BFMTV SD
+http://sonyiptv.net:8080/ahm/1234/275791
+#EXTINF:-1,FR | PIWI+ SD
+http://sonyiptv.net:8080/ahm/1234/275792
+#EXTINF:-1,FR | ANIMAUX SD
+http://sonyiptv.net:8080/ahm/1234/275793
+#EXTINF:-1,FR | DISNEY JUNIOR SD
+http://sonyiptv.net:8080/ahm/1234/275794
+#EXTINF:-1,FR | NICKELODEON JUNIOR SD
+http://sonyiptv.net:8080/ahm/1234/275795
+#EXTINF:-1,FR | TIJI SD
+http://sonyiptv.net:8080/ahm/1234/275796
+#EXTINF:-1,FR | TELETOON+ SD
+http://sonyiptv.net:8080/ahm/1234/275797
+#EXTINF:-1,FR | TELETOON+1 SD
+http://sonyiptv.net:8080/ahm/1234/275798
+#EXTINF:-1,FR | DISNEY CHANNEL SD
+http://sonyiptv.net:8080/ahm/1234/275799
+#EXTINF:-1,FR | NICKELODEON SD
+http://sonyiptv.net:8080/ahm/1234/275800
+#EXTINF:-1,FR | CANAL J SD
+http://sonyiptv.net:8080/ahm/1234/275801
+#EXTINF:-1,FR | GULLI SD
+http://sonyiptv.net:8080/ahm/1234/275802
+#EXTINF:-1,FR | COMEDY CENTRAL SD
+http://sonyiptv.net:8080/ahm/1234/275803
+#EXTINF:-1,FR | J-ONE SD
+http://sonyiptv.net:8080/ahm/1234/275804
+#EXTINF:-1,FR | GAME ONE SD
+http://sonyiptv.net:8080/ahm/1234/275805
+#EXTINF:-1,FR | PLANETE+ SD
+http://sonyiptv.net:8080/ahm/1234/275806
+#EXTINF:-1,FR | M6 SD
+http://sonyiptv.net:8080/ahm/1234/275807
+#EXTINF:-1,FR | ARTE SD
+http://sonyiptv.net:8080/ahm/1234/275808
