f28776f747d1d7d5bd18eeb267a75d6dd68b5f6f
GolfiArtistryGamer <165471099+GolfiArtistryGamer@users.noreply.github.com>
2024-04-09 15:23:27 +0200

    Update vod-a.py

--- vod-a.py
+++ vod-a.py
@@ -14,4 +14,11 @@
 <externallink></externallink>
 </item>
 
+<item>
+<title>[COLOR lime][COLOR  yellow]••  [/COLOR]VOD[COLOR  yellow] •[COLOR  cyan] 3[/COLOR]</title>
+<thumbnail>https://projatol.000webhostapp.com/logo/vod.png</thumbnail>
+<link>ignoreme</link>
+<externallink></externallink>
+</item>
+
 </items>
